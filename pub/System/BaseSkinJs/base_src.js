921f5767edc5fdd98006a84aadba49c426c3766f
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-12-04 02:30:02 +0000

    Item10247: start of edit page, bottom bar
    git-svn-id: http://svn.foswiki.org/trunk/BaseSkin@13296 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- pub/System/BaseSkinJs/base_src.js
+++ pub/System/BaseSkinJs/base_src.js
@@ -1,80 +1,123 @@
 jQuery(document).ready(function($) {
 	
+	function smoothScroll($) {
+		var offset = $('.foswikiTopicActions').offset();
+		var topicActionsY = offset.top;
+		
+		var SCROLL_DURATION = 250;
+		var SCROLL_TOP = 20;
+		if (topicActionsY !== undefined) {
+			SCROLL_TOP = topicActionsY + 35;
+		}
+		var scrollOpts = {
+			reset: true,
+			lazy: true,
+			hash: true,
+			duration: SCROLL_DURATION,
+			axis: 'y',
+			offset: { top:(-SCROLL_TOP), left:0 },
+			easing: 'swing'
+		};
+		$('a').localScroll(scrollOpts);
+	
+		// also scroll on page load
+		var anchor = getAnchor();
+		if (anchor) {
+			var $target = $('a[name=' + anchor + ']');
+			if ($target.length === 0) {
+				$target = $('#' + anchor);
+			}
+			if ($target.length !== 0) {
+				$('body').scrollTo( $target, SCROLL_DURATION, scrollOpts );
+			}
+		}
+	}
+	
+	function fixTopicActions($, resize) {
+		
+		var $body = $('body');
+		var $fixed = $('.foswikiFixed');
+		if (!$fixed.length) {
+			return;
+		}
+		var $topicActions = $('.foswikiFixed .foswikiTopicActions');
+		if (!$topicActions.length) {
+			return;
+		}
+		var $topic = $('.foswikiMain');
+		
+		var offset = $fixed.offset();
+		var topicActionsY = offset.top;
+
+		var orgPadding = parseInt($topicActions.css('padding-top'), 10);
+		var topicPadding = parseInt($topic.css('padding-top'), 10);
+	
+		var MIN_PADDING = 4, hasFixed = 0, p, d, ival;
+		
+		var handleScroll = function() {
+
+			var scrollTop = $(window).scrollTop();
+			if (scrollTop >= topicActionsY) {
+				if (!hasFixed) {
+					$body.addClass('foswikiHasFixed');
+					var height = $fixed.outerHeight();
+					$topic.css('padding-top', topicPadding + height);
+					hasFixed = 1;
+				}
+	
+			} else {
+				if (hasFixed) {
+					$body.removeClass('foswikiHasFixed');
+					hasFixed = 0;
+					$topic.css('padding-top', topicPadding);
+				}
+			}
+			
+			if (resize) {
+				p = orgPadding + 0.5 * (topicActionsY - scrollTop);
+				if (p < MIN_PADDING ) {
+					p = MIN_PADDING;
+				} else if (p > orgPadding) {
+					p = orgPadding;
+				}
+				if (d !== p) {
+					d = p;
+					$topicActions.css('padding-top', d);
+					$topicActions.css('padding-bottom', d);
+				}
+			}
+		};
+		
+		var handleScrollEvent = function() {
+			clearTimeout(ival);
+			ival = setTimeout(function() {
+				handleScroll();
+			}, 5);
+		};
+		
+		$(window).scroll(handleScrollEvent);
+	}
+		
+	function getAnchor( url ) {
+		url = url || location.href;
+		return url.replace( /^[^#]*#?!?(.*)$/, '$1' );
+	}
 	$('input.foswikiFocus').livequery(function() {
 		$(this).focus();
 	});
 	
-	var INITIAL_TOPIC_ACTIONS_Y;
-	
-	// stick topic action to top
-	var offset = $('.foswikiTopicActions').offset();
-	INITIAL_TOPIC_ACTIONS_Y = offset.top;
-	var $foswikiTopicActions = $('.foswikiTopicActions');
-	var $body = $('body');
-	var hasFixedTopBar = 0;
-	
-	$(window).scroll(function() {
-    	if ($(window).scrollTop() >= INITIAL_TOPIC_ACTIONS_Y) {
-    		if (!hasFixedTopBar) {
-	        	$body.addClass('foswikiFixedTopBar');
-    	    	hasFixedTopBar = 1;
-    	    }
-		} else {
-			if (hasFixedTopBar) {
-				$body.removeClass('foswikiFixedTopBar');
-				hasFixedTopBar = 0;
-			}
-		}
-	});
-
-/*
-	$(window).scroll(function() {
-    	if ($(window).scrollTop() >= INITIAL_TOPIC_ACTIONS_Y) {
-        	$foswikiTopicActions.css({
-        		position: 'fixed',
-        		top: 0
-        	});
-		} else {
-			$foswikiTopicActions.css({
-				position: 'relative',
-				top: 0
-			});
-		}
-	});
-*/
-	var SCROLL_DURATION = 250;
-	var SCROLL_TOP = 20;
-	if (INITIAL_TOPIC_ACTIONS_Y !== undefined) {
-		SCROLL_TOP = INITIAL_TOPIC_ACTIONS_Y + 20;
-	}
-	var scrollOpts = {
-		reset: true,
-		lazy: true,
-		hash: true,
-		duration: SCROLL_DURATION,
-		axis: 'y',
-		offset: { top:(-SCROLL_TOP), left:0 },
-		easing: 'swing'
-	};
-	$('a').localScroll(scrollOpts);
-
-	// also scroll on page load
-	var anchor = getAnchor();
-	if (anchor) {
-		var $target = $('a[name=' + anchor + ']');
-		if ($target.length === 0) {
-			$target = $('#' + anchor);
-		}
-		if ($target.length !== 0) {
-			$('body').scrollTo( $target, SCROLL_DURATION, scrollOpts );
-		}
-	}
+	smoothScroll($);
+	fixTopicActions($, false);
 
 });
 
+function tinyMceInited() {
+	var $toolbar = jQuery('#topic_toolbargroup').parent().parent();
+	var $new = jQuery('<div class="mceEditor o2k7Skin o2k7SkinBlack">')
+		.append(jQuery('<table style="width: 100%;">')
+			.append($toolbar)
+		);
+	$new.appendTo('.foswikiContextual .foswikiContainer');
+}
 
 
-function getAnchor( url ) {
-	url = url || location.href;
-	return url.replace( /^[^#]*#?!?(.*)$/, '$1' );
-};
