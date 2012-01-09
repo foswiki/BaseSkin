e6c7a9c9571409c55e971614278b3d9a04c02ecb
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-01-09 10:53:58 +0000

    Item10247: update of ongoing work; adding webfonts
    git-svn-id: http://svn.foswiki.org/trunk/BaseSkin@13569 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- pub/System/BaseSkinJs/base_edit_src.js
+++ pub/System/BaseSkinJs/base_edit_src.js
@@ -1,31 +1,151 @@
-/**
-Called from TinyMCEPlugin init, defined in FoswikiTiny.init
+var foswiki;
+if (!foswiki) {
+	foswiki = {};
+}
+if (!foswiki.base) {
+	foswiki.base = {};
+}
+var FoswikiTiny,
+	tinymce;
 
-To make this work, the init topic is set like:
-   * Set TINYMCEPLUGIN_INIT_TOPIC = %SYSTEMWEB%.BaseSkinTinyMCEPlugin
+foswiki.base.edit = (function () {
 
-BaseSkinTinyMCEPlugin defines the init callback in init_instance_callback.
-*/
+	"use strict";
+
+	return {
+
+		/**
+		Called from TinyMCEPlugin init, defined in FoswikiTiny.init ("init_instance_callback").
+		*/
+		tinyMceInited: function () {
+
+			if (!FoswikiTiny || !FoswikiTiny.init) {
+				return;
+			}
+
+			var $toolbar,
+				$container,
+				classes = [],
+				skin,
+				skinVariant;
+
+			$toolbar = jQuery('#topic_toolbargroup');
+			if ($toolbar.length === 0) {
+				return;
+			}
+
+			$container = jQuery('#foswikiEditControlsContainer');
+			if ($container.length === 0) {
+				return;
+			}
+
+			// grab the current class name to set it to the new container
+			classes = [];
+			skin = FoswikiTiny.init.skin || 'default';
+			classes.push(skin + 'Skin');
+
+			if (FoswikiTiny.init.skin_variant) {
+				skinVariant = FoswikiTiny.init.skin_variant.charAt(0).toUpperCase() + FoswikiTiny.init.skin_variant.slice(1);
+				classes.push(skin + 'Skin' + skinVariant);
+			}
+			$toolbar.addClass(classes.join(' '));
+
+			// move toolbar to our container
+			$toolbar.appendTo($container);
+
+
+			jQuery('body').addClass('foswikiHasWysiwyg');
+
+			// remove original toolbar table cells
+			jQuery('#topic_tbl .mceToolbar').remove();
+		},
+
+		fitToContent: function (elem, minAreaHeight) {
+
+			if (!elem) {
+				return;
+			}
+
+			var SAFE_PADDING = 15,
+				adjustedHeight,
+				elemMinHeight = document.documentElement.clientHeight - minAreaHeight,
+				nowysiwyg = parseInt(jQuery('form[name=main] [name="nowysiwyg"]').val(), 10);
+
+			if (jQuery('body.foswikiHasWysiwyg').length > 0) {
+				// we use the autoresize plugin from TineMCE,
+				// so don't continue with our own resizing
+				return;
+			}
+
+			// Account for deleted lines
+			if (elem.clientHeight <= elem.scrollHeight) {
+				elem.style.height = elemMinHeight + 'px';
+			}
+
+			adjustedHeight = Math.max(elem.scrollHeight, elem.clientHeight) + SAFE_PADDING;
+			if (adjustedHeight > elem.clientHeight) {
+				elem.style.height = adjustedHeight + 'px';
+			}
+		},
+
+		/**
+		Sets up the fit to content calls for the raw editor.
+		*/
+		initRaw: function () {
+			var nowysiwyg = parseInt(jQuery('form[name=main] [name="nowysiwyg"]').val(), 10),
+				$textarea = jQuery('.foswikiEditTextarea textarea'),
+				that = this;
+
+			if (nowysiwyg !== 1) {
+				jQuery('body').addClass('foswikiHasWysiwyg');
+			}
+
+			$textarea.livequery(function () {
+				that.fitTextArea();
+			}).livequery('focus blur keyup paste change', function () {
+				that.fitTextArea();
+			});
+		},
+
+		fitTextArea: function () {
+			var FOOTER_HEIGHT = jQuery('.foswikiFooter').outerHeight(),
+				MIN_AREA_HEIGHT = jQuery('.foswikiEditTextarea').position().top + FOOTER_HEIGHT,
+				$textarea = jQuery('.foswikiEditTextarea textarea');
+			this.fitToContent($textarea[0], MIN_AREA_HEIGHT);
+		}
+	};
+
+
+}());
+
 function tinyMceInited() {
-	if (!FoswikiTiny.init) {
-		return;
-	}
-	var classes = ['mceEditor'];
-	if (FoswikiTiny.init.skin) {
-		classes.push(FoswikiTiny.init.skin + 'Skin');
-	}
-	if (FoswikiTiny.init.skin && FoswikiTiny.init.skin_variant) {
-		classes.push(FoswikiTiny.init.skin + 'Skin' + FoswikiTiny.init.skin_variant.capitalize());
-	}
-
-	var $toolbar = jQuery('#topic_toolbargroup').parent().parent();
-	var $new = jQuery('<div class="' + classes.join(' ') +  '">')
-		.append(jQuery('<table style="width: 100%;">')
-			.append($toolbar)
-		);
-	$new.appendTo('.foswikiTopicActions .foswikiContainer');
+	"use strict";
+	foswiki.base.edit.tinyMceInited();
 }
 
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+function onTextAreaChanged() {
+	"use strict";
+	foswiki.base.edit.fitTextArea();
 }
+
+if (FoswikiTiny !== undefined) {
+
+	/**
+	Ideally, init_instance_callback is set in FoswikiTiny.init.
+	In case it's not, we set it here.
+	*/
+	if (!FoswikiTiny.init.init_instance_callback && !tinymce.settings.init_instance_callback) {
+		tinymce.settings.init_instance_callback = tinyMceInited;
+	}
+
+	// register callback to notify us when the text editor has changed
+	FoswikiTiny.transformCbs.push(onTextAreaChanged);
+}
+
+jQuery(document).ready(function ($) {
+	"use strict";
+	console.log("doc ready", foswiki.base.edit);
+	foswiki.base.edit.initRaw();
+});
+
+
--- pub/System/BaseSkinJs/base_src.js
+++ pub/System/BaseSkinJs/base_src.js
@@ -1,68 +1,120 @@
-jQuery(document).ready(function($) {
+var foswiki;
+if (!foswiki) {
+	foswiki = {};
+}
+
+/**
+Base script. Handles:
+- smooth scrolling
+- sticky behaviour
+*/
+
+
+foswiki.base = (function () {
+
+	"use strict";
+
+	var getAnchor;
 	
-	function smoothScroll() {
-		var SCROLL_DURATION = 250,
-			SCROLL_TOP = 20,
-			$fixedElements = $('.foswikiMakeSticky'),
-			offsetY = 0,
-			scrollOpts,
-			anchor,
-			$target,
-			i;
-			
-		for (i=0; i<$fixedElements.length; i=i+1) {
-			offsetY += jQuery($fixedElements[i]).outerHeight();
-		}
-		offsetY += SCROLL_TOP;
-		
-		scrollOpts = {
-			reset: true,
-			lazy: true,
-			hash: true,
-			duration: SCROLL_DURATION,
-			axis: 'y',
-			offset: { top:(-offsetY), left:0 },
-			easing: 'swing'
-		};
-		$('a').localScroll(scrollOpts);
-	
-		// also scroll on page load
-		anchor = getAnchor();
-		if (anchor) {
-			$target = $('a[name=' + anchor + ']');
-			if ($target.length === 0) {
-				$target = $('#' + anchor);
-			}
-			if ($target.length !== 0) {
-				$('body').scrollTo( $target, 1, scrollOpts );
-			}
-		}
-	}
-			
-	function getAnchor( url ) {
+	getAnchor = function (url) {
 		url = url || location.href;
-		return url.replace( /^[^#]*#?!?(.*)$/, '$1' );
-	}
-	$('input.foswikiFocus').livequery(function() {
+		return url.replace(/^[^#]*#?!?(.*)$/, '$1');
+	};
+
+	return {
+
+		smoothScroll: function ($) {
+			var SCROLL_DURATION = 250,
+				SCROLL_TOP = 20,
+				$fixedElements = $('.foswikiMakeSticky'),
+				offsetY = 0,
+				scrollOpts,
+				anchor,
+				$target,
+				i;
+
+			for (i = 0; i < $fixedElements.length; i = i + 1) {
+				offsetY += $($fixedElements[i]).outerHeight();
+			}
+			offsetY += SCROLL_TOP;
+
+			scrollOpts = {
+				reset: true,
+				lazy: true,
+				hash: true,
+				duration: SCROLL_DURATION,
+				axis: 'y',
+				offset: { top: (-offsetY), left: 0 },
+				easing: 'swing'
+			};
+			$('a').localScroll(scrollOpts);
+
+			// also scroll on page load
+			anchor = getAnchor();
+			if (anchor) {
+				$target = $('a[name=\'' + anchor + '\']');
+				if ($target.length === 0) {
+					$target = $('#' + anchor);
+				}
+				if ($target.length !== 0) {
+					$('body').scrollTo($target, 1, scrollOpts);
+				}
+			}
+		},
+		
+		removeYellowFromInputs: function($) {
+			if (navigator.userAgent.toLowerCase().indexOf('chrome') >= 0) {
+				var chromechk_watchdog = 0,
+					chromechk;
+				chromechk = setInterval(function() {
+					if ($('input:-webkit-autofill').length > 0) {
+						clearInterval(chromechk);
+						$('input:-webkit-autofill').livequery(function () {
+							var value = $(this).val(),
+								name = $(this).attr('name');
+							$(this).after(this.outerHTML).remove();
+							$('input[name=' + name + ']').val(value);
+						});
+					} else if (chromechk_watchdog > 20) {
+						clearInterval(chromechk);
+					}
+					chromechk_watchdog++;
+				}, 50);
+			}
+		}
+		
+	};
+}());
+
+jQuery(document).ready(function ($) {
+
+	foswiki.base.smoothScroll($);
+	foswiki.base.removeYellowFromInputs($);
+
+	// add focus to elements with class foswikiFocus
+	$('input.foswikiFocus').livequery(function () {
 		$(this).focus();
 	});
-	
-	smoothScroll();
+
+	// make elements with class foswikiMakeSticky stick to the window	
 	$('.foswikiMakeSticky').sticky({
 		cssclass: 'foswikiSticky'
 	});
 });
 
+/**
+Sticky plugin
+https://github.com/ArthurClemens/jquery-sticky-plugin
+*/
 
-(function( $ ){
+(function ($) {
+	"use strict";
+	$.fn.sticky = function (options) {
 
-	$.fn.sticky = function(options) {
-	
-		var settings = $.extend( {
-		  'cssclass' : 'sticky'
-		}, options);
-		
-		var $fixedElements = [],
+		var settings = $.extend({
+			'cssclass': 'sticky'
+		}, options),
+			$fixedElements = [],
 			$fixedElement,
 			offsetY = 0,
 			scrollTop = 0,
@@ -70,14 +122,16 @@ jQuery(document).ready(function($) {
 			i,
 			$this,
 			scrollZone,
+			fix,
+			unfix,
 			handleScroll,
 			handleScrollEvent,
 			// scroll wait duration depends if sticky element changes are within scroll zone
 			SCROLL_WAIT_FINE = 5,
 			SCROLL_WAIT_COURSE = 15,
 			scrollWait = SCROLL_WAIT_COURSE;
-	
-		this.each(function() {
+
+		this.each(function () {
 			$this = $(this);
 			// scrollY: y position from where to fixate
 			$this.scrollY = $this.position().top - offsetY;
@@ -85,61 +139,68 @@ jQuery(document).ready(function($) {
 			// stickyY: y position when being sticky
 			$this.stickyY = offsetY;
 			offsetY += $this.outerHeight();
-			
+
 			// scrollZone: to get finegrained scroll updates when sticking/unsticking happens
 			scrollZone = $this.scrollY + $this.outerHeight();
-			
+
 			$fixedElements.push($this);
 		});
-		
-		handleScroll = function() {
+
+		fix = function ($element) {
+			// fix element at y position stickyY
+			$element.addClass(settings.cssclass);
+			$element.css({
+				top: $element.stickyY + 'px'
+			});
+			// add a div to hold the padding the fixed element has just removed
+			$element.$div = jQuery('<div></div>').css({
+				height: $element.outerHeight()
+			});
+			$element.after($element.$div);
+			$fixedElement.isFixed = 1;
+		};
+
+		unfix = function ($element) {
+			$element.$div.remove();
+			$element.removeClass(settings.cssclass);
+			$element.css({
+				top: 'auto'
+			});
+			$fixedElement.isFixed = 0;
+		};
+
+		handleScroll = function () {
 
 			scrollTop = $(window).scrollTop();
-			for (i=0; i<$fixedElements.length; i=i+1) {
+			for (i = 0; i < $fixedElements.length; i = i + 1) {
 				$fixedElement = $fixedElements[i];
 
 				if (scrollTop >= $fixedElement.scrollY) {
 					if (!$fixedElement.isFixed) {
-						$fixedElement.isFixed = 1;
-						
-						// fix element at y position stickyY
-						$fixedElement.addClass(settings.cssclass);
-						$fixedElement.css({
-							top: $fixedElement.stickyY + 'px'
-						});			
-						// add a div to hold the padding the fixed element has just removed
-						$fixedElement.$div = jQuery('<div></div>').css({
-							height: $fixedElement.outerHeight()
-						});
-						$fixedElement.after($fixedElement.$div);
+						fix($fixedElement);
 					}
 				} else {
 					if ($fixedElement.isFixed) {
-						$fixedElement.isFixed = 0;
-						$fixedElement.$div.remove();
-						$fixedElement.removeClass(settings.cssclass);
-						$fixedElement.css({
-							top: 'auto'
-						});
+						unfix($fixedElement);
 					}
 				}
 			}
 			scrollWait = scrollTop < scrollZone ? SCROLL_WAIT_FINE : SCROLL_WAIT_COURSE;
 		};
-		
-		handleScrollEvent = function() {
+
+		handleScrollEvent = function () {
 			clearTimeout(ival);
-			ival = setTimeout(function() {
+			ival = setTimeout(function () {
 				handleScroll();
 			}, scrollWait);
 		};
-		
+
 		$(window).scroll(handleScrollEvent);
-		
+
 		// update first time without scroll event
 		handleScroll();
-		
-		return this;	
+
+		return this;
 	};
-	
-})(jQuery);
+
+}(jQuery));
